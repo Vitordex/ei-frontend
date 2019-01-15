0bc471fa5bcfa5f6afd84e879153ba825b37be1b
Vitor Ribeiro <vitordexjesus@gmail.com>
2019-01-15 13:21:23 -0200

    feat(app): Transform into frontend app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatProgressSpinnerModule, MatButtonModule, MatCardModule, MatTooltipModule, MatSnackBarModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChangePasswordService } from './change-password-service/change-password.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangePasswordFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'change/password', component: ChangePasswordFormComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    ChangePasswordService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
