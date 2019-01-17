1b6ac36424f5291e56fa4ee4b265dc86d38dfd22
Vitor Ribeiro <vitordexjesus@gmail.com>
2019-01-16 22:04:14 -0200

    feat(app): Add generic items service for materials, equipments and labors

---
+++ src/app/items-services/item.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  private baseUrl = '';
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = window.location.origin;
+  }
+
+  public getItems(authToken: string, itemName: string): Promise<any>{
+    const url = `${this.baseUrl}/${itemName}/`;
+    const headers = new HttpHeaders({'x-authentication-token': authToken});
+    const options = { headers };
+
+    return this.http.get(url, options).toPromise();
+  }
+
+  public deleteItem(itemId: string, authToken: string, itemName: string){
+    const url = `${this.baseUrl}/${itemName}/${itemId}`;
+    const headers = new HttpHeaders({'x-authentication-token': authToken});
+    const options = { headers };
+
+    return this.http.delete(url, options).toPromise();
+  }
+}
