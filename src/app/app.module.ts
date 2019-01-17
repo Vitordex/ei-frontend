bbadae73dbc5473c7ae8cf66d339ae2b52290508
Vitor Ribeiro <vitordexjesus@gmail.com>
2019-01-16 22:03:15 -0200

    feat(app): Add imports to angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatProgressSpinnerModule, MatButtonModule, MatCardModule, MatTooltipModule, MatSnackBarModule } from '@angular/material';
+import {
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatButtonModule,
+  MatCardModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatPaginatorModule,
+  MatTabsModule,
+  MatBottomSheetModule,
+  MatTableModule
+} from '@angular/material';
+
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ChangePasswordService } from './change-password-service/change-password.service';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './auth/auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Route[] = [
+  {
+    path: 'change/password', component: ChangePasswordFormComponent
+  },
+  {
+    path: 'items', component: ItemsComponent//, canActivate: [AuthGuardService]
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChangePasswordFormComponent
+    ChangePasswordFormComponent,
+    ItemsComponent,
+    HomeComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -25,13 +61,13 @@ import { RouterModule } from '@angular/router';
     MatCardModule,
     MatTooltipModule,
     MatSnackBarModule,
+    MatPaginatorModule,
+    MatTabsModule,
+    MatBottomSheetModule,
+    MatTableModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: 'change/password', component: ChangePasswordFormComponent
-      }
-    ])
+    RouterModule.forRoot(routes)
   ],
   providers: [
     ChangePasswordService
