e21a52d51fa6eb7fedf15cfced106c3f5378d97f
Vitor Ribeiro <vitordexjesus@gmail.com>
2019-01-17 22:16:22 -0200

    feat(app): Add post method to items service

--- src/app/items-services/item.service.ts
+++ src/app/items-services/item.service.ts
@@ -14,19 +14,33 @@ export class ItemService {
 
   public getItems(authToken: string, itemName: string): Promise<any> {
     const url = `${this.baseUrl}/${itemName}/`;
+    
     const headers = new HttpHeaders({ [Constants.TOKEN_HEADER]: authToken });
 
-    return this.http.get(url, { 
-      headers, 
-      observe: 'body' 
+    return this.http.get(url, {
+      headers,
+      observe: 'body'
     }).toPromise();
   }
 
   public deleteItem(itemId: string, authToken: string, itemName: string) {
     const url = `${this.baseUrl}/${itemName}/${itemId}`;
+    
     const headers = new HttpHeaders({ [Constants.TOKEN_HEADER]: authToken });
     const options = { headers };
 
     return this.http.delete(url, options).toPromise();
+  }
+
+  public postFile(file: File, authToken: string, itemName: string) {
+    const url = `${this.baseUrl}/${itemName}/`;
+    
+    const headers = new HttpHeaders({ [Constants.TOKEN_HEADER]: authToken });
+    const options = { headers };
+
+    const body: FormData = new FormData();
+    body.append('file', file, file.name);
+
+    return this.http.post(url, body, options).toPromise();
   }
 }
